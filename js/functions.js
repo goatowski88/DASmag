50c442e60e3c0ee7a480072290f5c20b9e9c2d0e
Tomek Goatowski <tomaszkozlowski.tech@gmail.com>
2022-07-24 19:30:02 +0200

    Add raw table

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+$("[data-toggle='myCollapse']").click(function( ev ) {
+    ev.preventDefault();
+    var target;
+    if (this.hasAttribute('data-target')) {
+        target = $(this.getAttribute('data-target'));
+    } else {
+        target = $(this.getAttribute('href'));
+    };
+    target.toggleClass("in");
+    console.log(target.hasClass('in'));
+});
